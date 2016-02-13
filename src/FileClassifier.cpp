b83aac8068acdbc877d4cb915bc719f6d0bab6d3
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-02-13 21:14:33 +0300

    Cosmetics. Added debug output

--- src/FileClassifier.cpp
+++ src/FileClassifier.cpp
@@ -8,6 +8,7 @@ getFileGroups(std::string const &file_path)
 {
     createFilesList(file_path);
     indexFiles();
+    processFiles();
     return std::vector< std::string >();
 }
 
@@ -64,14 +65,15 @@ indexFiles()
 
         std::for_each(range.first
                     , range.second
-                    , [&] (Files::value_type &v)
+                    , [&] (Files::value_type const &v)
                       {
                           markedFiles_.insert(
                                   std::make_pair(currentFileId_, v.second));
+                          std::cout << "Inserted!" << std::endl;
                       });
         currentFileId_++;
 
-        std::cout << it->first << std::endl;
+        std::cout << "current file id = " << currentFileId_ << std::endl;
     }
 }
 
@@ -90,12 +92,25 @@ processFiles()
     }
 
     // TODO: iterator throw result
+    for_each(result.begin(), result.end(),
+            [&] (Files::value_type const &v)
+            {
+            std::cout << v.first << "# "
+                      << v.second->file_path.string() << std::endl;
+            });
 }
 
 void
 FileClassifier::
 processEqualSizeFiles(FilesRange const &sizeEqualRange, Files &output)
 {
+    for_each(sizeEqualRange.first, sizeEqualRange.second,
+            [&] (Files::value_type const &v)
+            {
+            std::cout << "[EL]: " << v.second->file_path.string() << std::endl;
+            });
+    std::cout << std::endl;
+
     Files filesIdGroups(sizeEqualRange.first, sizeEqualRange.second);
     Files tmpFilesIdGroups;
     std::size_t file_size = (sizeEqualRange.first)->second->size;
@@ -142,76 +157,13 @@ separateByNextByte(FilesRange const &range, Files &output)
     Files byteSeparatedFiles;
     for_each(range.first,
              range.second,
-             [&] (Files::value_type v)
+             [&] (Files::value_type const &v)
              {
                  std::uint8_t byte;
-                 FilePtr &file = v.second;
+                 FilePtr const &file = v.second;
                  file->input_stream.read((char *)&byte, sizeof(byte));
                  byteSeparatedFiles.insert(make_pair(byte, v.second));
              });
 
     addFilesByGroups(byteSeparatedFiles, output);
 }
-
-/*void
-FileClassifier::
-divideToUniqueGroups(LabelledFiles &size_equal_files)
-{
-
-    LabelledFilesRange range;
-    std::size_t identificator_counter = 0;
-    LabelledFiles result;
-
-    identificator_counter = 0;
-    bool eof_flag = false;
-
-    for (auto it = size_equal_files.begin();
-            it != size_equal_files.end() && !eof_flag;
-            it = range.second)
-    {
-        range = size_equal_files.equal_range(it->first);
-
-        // Process equal range
-        LabelGroup const &byteSeparatedFiles = separateByNextByte(range); 
-    }
-}
-
-LabelGroup
-FileClassifier::
-separateByNextByte(LabelledFilesRange const &range)
-{
-    LabelGroup byteSeparatedFiles;
-    for_each(range.first,
-             range.second,
-             [&] (LabelledFiles::value_type v)
-             {
-                 std::uint8_t byte;
-                 FilePtr &file = v.second;
-                 file->input_stream.read((char *)&byte, sizeof(byte));
-                 byteSeparatedFiles.insert(make_pair(byte, v.second));
-             });
-
-    return byteSeparatedFiles;
-}
-
-void
-FileClassifier::
-addByteSeparatedFiles(LabelGroup const &byteSeparatedFiles, LabelledFiles &result)
-{
-    std::pair< LabelGroup::const_iterator, LabelGroup::const_iterator > range;
-    Label currentLabel;
-
-    for (auto it = byteSeparatedFiles.cbegin();
-            it != byteSeparatedFiles.cend();
-            it = range.second)
-    {
-        range = byteSeparatedFiles.equal_range(it->first);
-        //currentLabel.id = 
-        for_each(range.first
-               , range.second
-               , [&] (LabelGroup::value_type const &v)
-                {
-                    result.insert(std::make_shared(currentLabel, v.second));
-                });
-    }
-}*/
